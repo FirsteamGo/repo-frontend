4098ada4db3344fef6f5ebb13d4e809299dd7b8e
Haytham <qaz7954200@gmail.com>
2023-03-08 01:14:38 +0800

    work in sideBar

--- FirstTeamVue/src/views/YeluPages/Shop/ShopView.vue
+++ FirstTeamVue/src/views/YeluPages/Shop/ShopView.vue
@@ -12,8 +12,10 @@ import bg0 from "@/assets/img/bg9.jpg";
 import Typed from "typed.js";
 
 //sections 放置要引入的頁面
+import carousel from "../Shop/shopsections/carousel.vue";
+import product from "../Shop/shopsections/product.vue";
+import shopnav from "../Shop/shopsections/shopnav.vue";
 // import Information from "./Sections/AboutInformation.vue";
-
 
 const body = document.getElementsByTagName("body")[0];
 //hooks
@@ -48,37 +50,50 @@ onUnmounted(() => {
     }"
     transparent
   />
+
   <header class="bg-gradient-dark">
     <div
-      class="page-header min-vh-50
-"
+      class="page-header min-vh-50"
       :style="{ backgroundImage: `url(${bg0})` }"
     >
       <span class="mask bg-gradient-dark opacity-6"></span>
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-lg-8 text-center mx-auto my-auto">
-            <h1 class="text-white">
-              Title 
-            </h1>
-           
-            <p class="lead mb-4 text-white opacity-8">
-              We’re constantly trying to express ourselves and actualize our
-              dreams. If you have the opportunity to play this game
+            <h1 class="text-white">YELU Shop</h1>
+
+            <p class="lead mb-4 text-white opacity-10">
+              In YELU, you can find any camping equipment you need here!
             </p>
-            
-           
           </div>
         </div>
       </div>
     </div>
   </header>
-  <div class="card card-body shadow-xl mx-3 mx-md-4 mt-n6">
 
+  <div class="card card-body shadow-xl mx-3 mx-md-4 mt-n6">
     <!-- 記得這裡設定引入標籤 -->
     <!-- <Information /> -->
- 
-
+    <div class="common-layout">
+      <el-header class="head"><carousel /></el-header>
+      <el-container>
+        <el-container class="item1">
+          <el-aside class="aside" width="370px"><shopnav /></el-aside>
+          <el-main class="main"><product /></el-main>
+        </el-container>
+      </el-container>
+    </div>
   </div>
+
   <yelufooter />
 </template>
+<style>
+.head {
+  margin-bottom: 100px;
+}
+.item1 {
+  box-sizing: border-box;
+  display: flex; /* 或 display: grid; */
+  justify-content: space-between; /* 可以調整 item 之間的間距和對齊方式 */
+}
+</style>
