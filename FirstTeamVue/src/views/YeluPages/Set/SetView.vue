f17b05f2bb6d1765d22cba02d0408f0f7934238b
shihjunren <eric25249@gmail.com>
2023-03-10 09:33:24 +0800

    00

--- FirstTeamVue/src/views/YeluPages/Set/SetView.vue
+++ FirstTeamVue/src/views/YeluPages/Set/SetView.vue
@@ -13,7 +13,7 @@ import bg0 from "./img/01.jpeg";
 //dep
 import Typed from "typed.js";
 import Layout from "./Sections/Layout.vue";
-import Testtable1 from "./Sections/testtable1.vue";
+import Testtable from "./Sections/testtable.vue";
 
 //sections 放置要引入的頁面
 // import Information from "./Sections/AboutInformation.vue";
@@ -44,36 +44,30 @@ onUnmounted(() => {
 });
 </script>
 <template>
-  <DefaultNavbar
-    :action="{
-      route: 'javascript:;',
-      label: 'Buy Now',
-      color: 'btn-white',
-    }"
-    transparent
-  />
+  <DefaultNavbar :action="{
+    route: 'javascript:;',
+    label: 'Buy Now',
+    color: 'btn-white',
+  }" transparent />
   <header class="bg-gradient-dark">
-    <div
-      class="page-header min-vh-50
-"
-      :style="{ backgroundImage: `url(${bg0})` }"
-    >
+    <div class="page-header min-vh-50
+  " :style="{ backgroundImage: `url(${bg0})` }">
       <span class="mask bg-gradient-dark opacity-6"></span>
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-lg-8 text-center mx-auto my-auto">
             <h1 class="text-white">
-              套裝行程 
+              套裝行程
             </h1>
-           
+
             <p class="lead mb-4 text-white opacity-8">
               不用規劃，直接拎包入住! <br>
               走進大自然，享受真正的自由！ <br>
               離開城市，找到屬於自己的寧靜空間！ <br>
               套裝行程，可以幫你完成你的夢想！ <br>
             </p>
-            
-           
+
+
           </div>
         </div>
       </div>
@@ -81,12 +75,12 @@ onUnmounted(() => {
   </header>
 
 
- <Breadcrumb/>
+  <Breadcrumb />
 
-  <div class="card card-body shadow-xl mx-3 mx-md-4 mt-n6">  
-   
-   <Layout/>
-   <Testtable1 />
+  <div class="card card-body shadow-xl mx-3 mx-md-4 mt-n6">
+
+    <!-- <Layout/> -->
+    <Testtable />
 
   </div>
   <yelufooter />
