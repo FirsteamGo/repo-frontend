a544558963341bb12217af35c139445aab87df06
Meazou <eo.s@gmail.com>
2023-03-16 15:48:02 +0800

    window basic component finished

--- FirstTeamVue/src/views/YeluPages/Shop/SPA/con1_1.vue
+++ FirstTeamVue/src/views/YeluPages/Shop/SPA/con1_1.vue
@@ -10,33 +10,46 @@ import card2 from "../shopcards/card2.vue";
 
 const webApiBaseAddr = ref("https://localhost:7108/api/ShopDetails");
 const imagelist = ref("https://localhost:7120/images/");
-let shopPro = reactive([]);
+const shopPro = reactive([]);
 const visible=ref(false)
 const image=ref('')
-const shopItem=reactive([])
+let shopItem=reactive([])
+const 單價=ref('')
+const 產品名稱=ref('')
 
-const getEmployeeDTOes = onMounted(() => {
-  axios
-    .get(webApiBaseAddr.value)
-    .then((res) => {
-      //console.log(res.data);
-      shopPro.splice(0, res.data.length, ...res.data);
-      console.log(shopPro);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-let datail=(商品細項ID)=>{
-  var shopItem=[]
+
+onMounted(async()=>{
+  await axios.get(webApiBaseAddr.value)
+  .then(res=>{
+    // for(let i=0;i<res.data.length;i++){
+    //   let item=res.data[i];
+    //   item.Edit=false;
+    //   console.log(item);
+    // }
+    shopPro.splice(0,res.data.length,...res.data)
+    console.log(shopPro);
+
+  }).catch(err=>{
+    console.log(err);
+  })
+}) 
+
+let detail=(商品細項ID)=>{
+  let shopItem=[]
+  //alert(商品細項ID)
   for(let i=0;i<shopPro.length;i++){
-    var item=shopPro[i]
-    if(item.商品細項ID=商品細項ID){
+    let item=shopPro[i]
+    if(item.商品細項id==商品細項ID){
       單價.value=item.單價;
+      //console.log(單價.value);
       產品名稱.value=item.產品名稱;
-      image.value=item.產品圖片;
+      //console.log(產品名稱.value);
+
     }
+    else{item.Edit=false;}
+    shopItem.push(item);
   }
+  //shopPro.splice(0,shopItem.length,...shopItem)
 }
 </script>
 <template>
@@ -45,22 +58,30 @@ let datail=(商品細項ID)=>{
       <el-main>
         <section class="py-3">
           <div class="row">
-            <div class="col-lg-2 col-sm-6" v-for="item in shopPro">
+            <div class="col-lg-2 col-sm-6" v-for="item in shopPro" >
               <card2
                 :image="`${imagelist}${item.產品圖片}`"
                 :title="item.產品名稱"
                 :description="item.單價"
                 :content="item.產品說明"              
               />
-              <el-button @click="{datail(item.商品細項ID);visible=true}" class="datail" type="danger">123</el-button>
+              <el-button @click="{detail(item.商品細項id);visible=true}" class="detail" type="danger">123</el-button>
               <div>
-                <el-dialog v-model="visible" title="這是個示範" width="50%" draggable>
+              </div>
+            </div>
+            <el-dialog v-model="visible" title="這是個示範" width="50%" draggable>
                 <div class="modal-header">
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <span class="dialog-footer">
                   <div>
-                    <img :src="`${imagelist}${image}`" alt="empty">
+                    <label class="form-label">價錢:{{ 單價 }}</label>
+                  </div>
+                  <div>
+                    <label class="form-label">名稱:{{ 產品名稱 }}</label>
+                  </div>
+                  <div>
+            <el-input-number v-model="quantity" :min="1" :max="10" :step="1"></el-input-number>
                   </div>
                   <el-button @click="visible = false">取消</el-button>
               <el-button type="primary" @click="  visible = false ">
@@ -68,8 +89,6 @@ let datail=(商品細項ID)=>{
               </el-button>
                 </span>
                 </el-dialog>
-              </div>
-            </div>
           </div>
         </section>
       </el-main>
