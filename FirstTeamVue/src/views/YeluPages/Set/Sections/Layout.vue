194170f70dd2f51206e04a63d4f2da7cdd06b888
shihjunren <eric25249@gmail.com>
2023-03-21 15:20:18 +0800

    0321更新

--- FirstTeamVue/src/views/YeluPages/Set/Sections/Layout.vue
+++ FirstTeamVue/src/views/YeluPages/Set/Sections/Layout.vue
@@ -2,60 +2,57 @@
 <!-- 步驟條 -->
 <script  setup>
   // import card1 from '../Self/Sections/card.vue'
- 
-  </script>
+
+</script>
 
 <script >
-  import step1 from './SetOD.vue'
-  import step2 from './SetO.vue'
-  import step3 from './SetShop.vue'
-  
-  import { ref,onMounted,reactive } from 'vue'
-  import { RouterLink } from "vue-router";
-  
-  const active = ref(0)
-    
-  export default {
-    components: {
-      step1,
-      step2,
-      step3
-    },
-    data() {
-      return {        
-        content: 'step1',
-        active: 0,
-      }
-    },
-     methods: {
-      
-      next() {      
+import step1 from './SetOD.vue'
+import step2 from './SetO.vue'
+import step3 from './SetShop.vue'
 
-      if(active.value ==0){
-        const member =JSON.parse(localStorage.getItem('customerData'))
-        if(!member){
-          alert("麻煩請登入會員");
-          console.log("麻煩請登入會員")
-          return ;
-        }
-      }else if(active.value==1){
-        const setodata = JSON.parse(localStorage.getItem('setorderdetail'))
-        if(!setodata){
-          alert("麻煩請選擇套裝行程方案!");
-          console.log("麻煩請選擇套裝行程方案")
+import { ref, onMounted, reactive } from 'vue'
+import { RouterLink } from "vue-router";
+
+const active = ref(0)
+
+export default {
+  components: {
+    step1,
+    step2,
+    step3
+  },
+  data() {
+    return {
+      content: 'step1',
+      active: 0,
+    }
+  },
+  methods: {
+
+    next() {
+
+      if (active.value == 0) {
+        const member = JSON.parse(localStorage.getItem('customerData'))
+        const SetOD = JSON.parse(localStorage.getItem('setorderdetail'))
+        if (!member || !SetOD) {
+          alert("麻煩請登入會員與選擇其一套裝行程");
+          // console.log("麻煩請登入會員")
           return;
         }
-      }else if(active.value==2){
+      } else if (active.value == 1) {
         const setodData = JSON.parse(localStorage.getItem('setorder'))
-        if(!setodData){
+        if (!setodData) {
           alert("麻煩請填寫下列所需資訊及確認套裝形成方案!");
           console.log("麻煩請填寫下列所需資訊及確認套裝形成方案")
           return;
         }
+      } else if (active.value == 2) {
+
+
       }
       active.value++;
       if (this.active === 3) {
-        this.active = 0;        
+        this.active = 0;
       }
       this.content = `step${active.value + 1}`;
     },
@@ -69,30 +66,31 @@
         this.content = 'step3';
       }
     }
-  }}
+  }
+}
 </script>
 
 
 <template>
-<!-- 步驟條 -->
-<div class="container mt-5 px-5">
-  <el-steps :active="active" finish-status="success" >
-    <el-step title="第一步: 套裝行程" />
-    <el-step title="第二步: 填寫詳細資訊" />
-    <el-step title="第三步: 訂單確認" />
-  </el-steps>
-  
-  <div style="margin-top: 12px">
-    <el-button v-if="active > 0" @click="back">上一步</el-button>
-    <el-button v-if="active < 2" @click="next" >下一步</el-button> 
-  
+  <!-- 步驟條 -->
+  <div class="container mt-5 px-5">
+    <el-steps :active="active" finish-status="success">
+      <el-step title="第一步: 套裝行程" />
+      <el-step title="第二步: 填寫詳細資訊" />
+      <el-step title="第三步: 訂單確認" />
+    </el-steps>
+
+    <div style="margin-top: 12px">
+      <el-button v-if="active > 0" @click="back">上一步</el-button>
+      <el-button v-if="active < 2" @click="next">下一步</el-button>
+
+    </div>
+
+    <p></p>
+
+    <component :is="content"></component>
+
   </div>
-  
-  <p></p>
-
-  <component :is="content"></component>
-
-</div>
 </template>
 
 
@@ -121,12 +119,15 @@
   margin-bottom: -1px;
   margin-right: -1px;
 }
+
 .tab-button:hover {
   background: #e0e0e0;
 }
+
 .tab-button.active {
   background: #e0e0e0;
 }
+
 .tab {
   border: 1px solid #ccc;
   padding: 10px;
--- FirstTeamVue/src/views/YeluPages/Set/Sections/SetO.vue
+++ FirstTeamVue/src/views/YeluPages/Set/Sections/SetO.vue
@@ -1,186 +1,181 @@
 <template>
-  <el-button type="primary" round style="width: 15%;margin:20px;"  @click="Setdetail=true">套裝行程詳細資訊</el-button>
+  <el-button type="primary" round style="width: 15%;margin:20px;" @click="Setdetail = true">套裝行程詳細資訊</el-button>
 
   <div class="common-layout">
     <el-container style="height: 500px;">
-      <el-aside width="60%" style="display: flex;justify-content: center;align-items: center;">
-        <weather/>
+      <el-aside width="50%" style="display: flex;justify-content: center;align-items: center;">
+        <weather />
       </el-aside>
       <el-main>
         <el-form :model="form" label-width="120px">
-      
-      <el-form-item label="訂單編號"  hidden>{{setordernu}}</el-form-item>
 
-      <el-form-item label="露營日期" class="demonstration">
-           <el-date-picker
-              v-model="form.data"
-              type="daterange"
-              range-separator="To"
-              start-placeholder="開始日期"
-              end-placeholder="結束日期"
-              size="default"
-              /></el-form-item>         
-      <el-form-item label="預計人數 :">
-        <el-input type="text" v-model="form.numberOfPeople" placeholder="請填入參加人數"  />
-      </el-form-item>        
-       <el-form-item label="合計總價 :" >{{ totalPrice }} 元</el-form-item>
+          <el-form-item label="訂單編號：" hidden>{{ setordernu }}</el-form-item>
+          <el-form-item label="套裝方案：">{{ setod.套裝方案 }}</el-form-item>
+
+          <el-form-item label="露營日期：" class="demonstration">
+            <el-date-picker v-model="form.data" type="daterange" range-separator="To" start-placeholder="開始日期"
+              end-placeholder="結束日期" size="default" /></el-form-item>
+          <el-form-item label="預計人數 :">
+            <el-input type="text" v-model="form.numberOfPeople" placeholder="請填入參加人數" />
+          </el-form-item>
+          <el-form-item label="合計總價 :">{{ totalPrice }} 元</el-form-item>
 
 
-      <el-form-item label="評論 :" style="width: 100%;">
-        <el-input v-model="form.評論" type="textarea" />
-      </el-form-item>
-      <el-form-item label="評分 :">
-        <el-select v-model="form.評分" placeholder="請評分謝謝">
-          <el-option label="1" value="1" />
-          <el-option label="2" value="2" />
-          <el-option label="3" value="3" />
-          <el-option label="4" value="4" />
-          <el-option label="5" value="5" />
-        </el-select>
-      </el-form-item>
+          <el-form-item label="評論 :" style="width: 100%;">
+            <el-input v-model="form.評論" type="textarea" />
+          </el-form-item>
+          <el-form-item label="評分 :">
+            <el-select v-model="form.評分" placeholder="請評分謝謝">
+              <el-option label="1" value="1" />
+              <el-option label="2" value="2" />
+              <el-option label="3" value="3" />
+              <el-option label="4" value="4" />
+              <el-option label="5" value="5" />
+            </el-select>
+          </el-form-item>
 
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">確認</el-button>        
-      </el-form-item>
-    </el-form>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">確認</el-button>
+          </el-form-item>
+        </el-form>
       </el-main>
     </el-container>
 
   </div>
-    
 
-    <div>
-          <el-dialog v-model="Setdetail" title="套裝方案詳細資訊" width="50%" draggable>
-            <div class="modal-header">
 
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <span class="dialog-footer">
-              <div>
-                <img :src="`${mvc}${setod.活動圖片}`" >
-              </div>
-              <div>
-                <label class="form-lable">營區地區: {{ setod.地區 }}</label>
-              </div>
-              <div>
-                <label class="form-lable">營區縣市:{{ setod.縣市 }}</label>
-              </div>
-              <div>
-                <label class="form-lable">營區名稱: {{ setod.營區名稱 }}</label>
-              </div>
-              <div>
-                <label class="form-lable">露營方式: {{ setod.項目內容 }}</label>
-              </div>
-              <div>
-                <label class="form-lable">相關活動: {{ setod.活動名稱 }}</label>
-              </div>
-              <div>
-                <label class="form-lable">方案: {{ setod.套裝方案}}</label>                             
-              </div>
-              <div>
-                <label class="form-lable">細項: {{ setod.套裝細項 }}</label>
-              </div>
-              <div>
-                <label class="form-lable">單價: {{ setod.套裝行程價格 }} 元 / 人</label>
-              </div>
-              
-            </span>
+  <div>
+    <el-dialog v-model="Setdetail" title="套裝方案詳細資訊" width="50%" draggable>
+      <div class="modal-header">
 
-          </el-dialog>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <span class="dialog-footer">
+        <div>
+          <img :src="`${mvc}${setod.活動圖片}`">
         </div>
-  </template>
+        <div>
+          <label class="form-lable">營區地區: {{ setod.地區 }}</label>
+        </div>
+        <div>
+          <label class="form-lable">營區縣市:{{ setod.縣市 }}</label>
+        </div>
+        <div>
+          <label class="form-lable">營區名稱: {{ setod.營區名稱 }}</label>
+        </div>
+        <div>
+          <label class="form-lable">露營方式: {{ setod.項目內容 }}</label>
+        </div>
+        <div>
+          <label class="form-lable">相關活動: {{ setod.活動名稱 }}</label>
+        </div>
+        <div>
+          <label class="form-lable">方案: {{ setod.套裝方案 }}</label>
+        </div>
+        <div>
+          <label class="form-lable">細項: {{ setod.套裝細項 }}</label>
+        </div>
+        <div>
+          <label class="form-lable">單價: {{ setod.套裝行程價格 }} 元 / 人</label>
+        </div>
+
+      </span>
+
+    </el-dialog>
+  </div>
+</template>
   
-  <script  setup>
-  import { values } from 'lodash';
-  import axios from 'axios';
-  import { reactive,ref,computed,onMounted } from 'vue';
-  import weather from './weather.vue';
-  const webApi = ref("https://localhost:7108/api/SetOrderDetails");
-  const mvc = ref("https://localhost:7120/images/");
-  const now = new Date();
-  const Setdetail = ref(false)
-  const SetODID = reactive([])
-  
-  const setod= JSON.parse(localStorage.getItem('setorderdetail'))  
-  const meda= JSON.parse(localStorage.getItem('customerData'))
- 
-  const pricePerPerson=ref(setod.套裝行程價格)
-  const Seto = reactive([])
-  const 套裝行程id=ref(0)
-  const 會員id=ref(0)
-  
-  // do not use same name with ref
-  const form = reactive({        
-    訂單編號: '',
-    評論: '',
-    評分: '',
-    data: [],  
-    預計人數: 0,
-    合計總價:0,  
-  })
-  const gitpricePerPerson = onMounted(()=>{
-     axios.get(webApi.value)
-    .then(res => {    
-          
+<script  setup>
+import { values } from 'lodash';
+import axios from 'axios';
+import { reactive, ref, computed, onMounted } from 'vue';
+import weather from './weather.vue';
+const webApi = ref("https://localhost:7108/api/SetOrderDetails");
+const mvc = ref("https://localhost:7120/images/");
+const now = new Date();
+const Setdetail = ref(false)
+const SetODID = reactive([])
+
+const setod = JSON.parse(localStorage.getItem('setorderdetail'))
+const meda = JSON.parse(localStorage.getItem('customerData'))
+
+const pricePerPerson = ref(setod.套裝行程價格)
+const Seto = reactive([])
+const 套裝行程id = ref(0)
+const 會員id = ref(0)
+
+// do not use same name with ref
+const form = reactive({
+  訂單編號: '',
+  評論: '',
+  評分: '',
+  data: [],
+  預計人數: 0,
+  合計總價: 0,
+})
+const gitpricePerPerson = onMounted(() => {
+  axios.get(webApi.value)
+    .then(res => {
+
       SetODID.splice(0, res.data.length, ...res.data);
     })
     .catch((err) => {
       console.error(err);
     })
-  })
-  
-
-  const onSubmit = () => {
-
-    console.log('submit!')
-    const setordernu = `Set${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2, "0")}${now.getDate().toString().padStart(2, "0")}${now.getHours().toString().padStart(2, "0")}${now.getMinutes().toString().padStart(2, "0")}${now.getSeconds().toString().padStart(2, "0")}`;
-    form.訂單編號=setordernu;
-    form.預計人數 = form.numberOfPeople;
-    form.合計總價 = totalPrice.value; 
-    console.log(form);
-    let Seto = JSON.stringify(form)
-
-    localStorage.setItem('setorder', Seto)
-    alert("請按下一步進入購物車")
+})
 
 
-  }
+const onSubmit = () => {
 
+  console.log('submit!')
   const setordernu = `Set${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2, "0")}${now.getDate().toString().padStart(2, "0")}${now.getHours().toString().padStart(2, "0")}${now.getMinutes().toString().padStart(2, "0")}${now.getSeconds().toString().padStart(2, "0")}`;
+  form.訂單編號 = setordernu;
+  form.預計人數 = form.numberOfPeople;
+  form.合計總價 = totalPrice.value;
+  console.log(form);
+  let Seto = JSON.stringify(form)
+
+  localStorage.setItem('setorder', Seto)
+  alert("請按下一步進入購物車")
 
 
-  const formatDate = (dateString)=> {
-      const date = new Date(dateString);
-      const year = date.getFullYear();
-      const month = (date.getMonth() + 1).toString().padStart(2, "0");
-      const day = date.getDate().toString().padStart(2, "0");
-      return `${year}-${month}-${day}`;
-    }
+}
 
-   
-    const totalPrice = computed(() => {
-    const people = parseInt(form.numberOfPeople);
-    const price = parseInt(pricePerPerson.value);
-    return isNaN(people) || isNaN(price) ? 0 : people * price;
-    });
+const setordernu = `Set${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2, "0")}${now.getDate().toString().padStart(2, "0")}${now.getHours().toString().padStart(2, "0")}${now.getMinutes().toString().padStart(2, "0")}${now.getSeconds().toString().padStart(2, "0")}`;
 
-    
-  </script>
-  <style scoped>
-  .el-carousel__item h3 {
-    display: flex;
-    color: #475669;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
-  }
-  
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-  
-  .el-carousel__item:nth-child(2n + 1) {
-    background-color: #d3dce6;
-  }
-  </style>
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+
+
+const totalPrice = computed(() => {
+  const people = parseInt(form.numberOfPeople);
+  const price = parseInt(pricePerPerson.value);
+  return isNaN(people) || isNaN(price) ? 0 : people * price;
+});
+
+
+</script>
+<style scoped>
+.el-carousel__item h3 {
+  display: flex;
+  color: #475669;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
   
