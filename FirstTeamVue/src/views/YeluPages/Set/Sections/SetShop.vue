58a2746c05df777e7d7dc44b65987942439ab7e9
shihjunren <eric25249@gmail.com>
2023-03-22 11:21:19 +0800

    更新購物車頁面

--- FirstTeamVue/src/views/YeluPages/Set/Sections/SetShop.vue
+++ FirstTeamVue/src/views/YeluPages/Set/Sections/SetShop.vue
@@ -35,23 +35,96 @@ td {
 
             <td>
               <h5 class="mt-3">會員資訊</h5>
-              <div><el-button type="info" class="mt-3 mb-2" plain @click="mem = true">詳細資訊</el-button></div>
+
+              <span class="dialog-footer">
+                <div>
+                  <img :src="`${mvc}${meda.照片}`">
+                </div>
+                <div>
+                  <label class="form-lable">會員姓名: {{ meda.姓名 }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">連絡信箱: {{ meda.連絡信箱 }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">電話號碼: {{ meda.電話號碼 }}</label>
+                </div>
+              </span>
             </td>
 
             <td>
               <h5 class="mt-3">套裝行程</h5>
-              <div><el-button type="info" class="mt-3 mb-2" plain @click="Setdetail = true">詳細資訊</el-button></div>
+
+              <span class="dialog-footer">
+                <div>
+                  <img :src="`${mvc}${setod.活動圖片}`">
+                </div>
+                <div>
+                  <label class="form-lable">營區地區: {{ setod.地區 }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">營區縣市:{{ setod.縣市 }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">營區名稱: {{ setod.營區名稱 }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">露營方式: {{ setod.項目內容 }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">相關活動: {{ setod.活動名稱 }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">方案: {{ setod.套裝方案 }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">細項: {{ setod.套裝細項 }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">單價: {{ setod.套裝行程價格 }} 元 / 人</label>
+                </div>
+
+              </span>
+
             </td>
 
             <td>
               <h5 class="mt-3">套裝行程規劃</h5>
-              <div><el-button type="info" class="mt-3 mb-2" plain @click="Setorderdetail = true">詳細資訊</el-button></div>
+
+              <span class="dialog-footer">
+
+                <div>
+                  <label class="form-lable">預計參加人數: {{ seto.numberOfPeople }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">入住時間:{{ formatDate(seto.data[0]) }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">退住時間:{{ formatDate(seto.data[1]) }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">合計總價: {{ seto.合計總價 }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">評分: {{ seto.評分 }}</label>
+                </div>
+                <div>
+                  <label class="form-lable">評論: {{ seto.評論 }}</label>
+                </div>
+              </span>
             </td>
 
             <td>
+
               <button class="btn btn-primary" @click="Del()">刪除</button>
               <button class="btn btn-warning" @click="insert()">確認送出訂單</button>
-              <router-link :to="{ name: 'presentation' }" class="btn btn-primary"> 返回首頁</router-link>
+              <router-link :to="{ name: 'presentation' }" class="btn btn-primary"> <span class="material-icons">
+                  home
+                </span></router-link>
+              <div>
+                <label style="font-weight: bolder; font-size: large; color: rgb(100,0,0);">若想修改請刪除後重新填寫訂單</label>
+              </div>
+
             </td>
 
           </tr>
@@ -61,101 +134,7 @@ td {
 
     <!-- Modal -->
 
-    <div>
-      <el-dialog v-model="mem" title="會員詳細資訊" width="50%" draggable>
-        <div class="modal-header">
 
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <span class="dialog-footer">
-          <div>
-            <img :src="`${mvc}${meda.照片}`">
-          </div>
-          <div>
-            <label class="form-lable">會員姓名: {{ meda.姓名 }}</label>
-          </div>
-          <div>
-            <label class="form-lable">連絡信箱: {{ meda.連絡信箱 }}</label>
-          </div>
-          <div>
-            <label class="form-lable">電話號碼: {{ meda.電話號碼 }}</label>
-          </div>
-
-
-        </span>
-
-      </el-dialog>
-    </div>
-
-    <div>
-      <el-dialog v-model="Setdetail" title="套裝方案詳細資訊" width="50%" draggable>
-        <div class="modal-header">
-
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <span class="dialog-footer">
-          <div>
-            <img :src="`${mvc}${setod.活動圖片}`">
-          </div>
-          <div>
-            <label class="form-lable">營區地區: {{ setod.地區 }}</label>
-          </div>
-          <div>
-            <label class="form-lable">營區縣市:{{ setod.縣市 }}</label>
-          </div>
-          <div>
-            <label class="form-lable">營區名稱: {{ setod.營區名稱 }}</label>
-          </div>
-          <div>
-            <label class="form-lable">露營方式: {{ setod.項目內容 }}</label>
-          </div>
-          <div>
-            <label class="form-lable">相關活動: {{ setod.活動名稱 }}</label>
-          </div>
-          <div>
-            <label class="form-lable">方案: {{ setod.套裝方案 }}</label>
-          </div>
-          <div>
-            <label class="form-lable">細項: {{ setod.套裝細項 }}</label>
-          </div>
-          <div>
-            <label class="form-lable">單價: {{ setod.套裝行程價格 }} 元 / 人</label>
-          </div>
-
-        </span>
-
-      </el-dialog>
-    </div>
-    <div>
-      <el-dialog v-model="Setorderdetail" title="套裝行程規劃詳細資訊" width="50%" draggable>
-        <div class="modal-header">
-
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <span class="dialog-footer">
-
-          <div>
-            <label class="form-lable">預計參加人數: {{ seto.numberOfPeople }}</label>
-          </div>
-          <div>
-            <label class="form-lable">入住時間:{{ formatDate(seto.data[0]) }}</label>
-          </div>
-          <div>
-            <label class="form-lable">退住時間:{{ formatDate(seto.data[1]) }}</label>
-          </div>
-          <div>
-            <label class="form-lable">合計總價: {{ seto.合計總價 }}</label>
-          </div>
-          <div>
-            <label class="form-lable">評分: {{ seto.評分 }}</label>
-          </div>
-          <div>
-            <label class="form-lable">評論: {{ seto.評論 }}</label>
-          </div>
-        </span>
-
-      </el-dialog>
-    </div>
   </div>
 </template>
 
@@ -167,9 +146,6 @@ const mvc = ref("https://localhost:7120/images/");
 const webApi = ref("https://localhost:7108/api/SetOrders");
 
 
-const mem = ref(false)
-const Setdetail = ref(false)
-const Setorderdetail = ref(false)
 
 const seto = JSON.parse(localStorage.getItem('setorder'))
 const setod = JSON.parse(localStorage.getItem('setorderdetail'))
