671c620dc752a511a92ef7c8d2bfe50b0cf14085
pandariceball <tealove6053@gmail.com>
2023-03-24 16:48:36 +0800

    0324

--- FirstTeamVue/src/views/YeluPages/Camp/CampView.vue
+++ FirstTeamVue/src/views/YeluPages/Camp/CampView.vue
@@ -41,19 +41,13 @@ onUnmounted(() => {
 });
 </script>
 <template>
-  <DefaultNavbar
-    :action="{
-      route: 'javascript:;',
-      label: 'Buy Now',
-      color: 'btn-white',
-    }"
-    transparent
-  />
+  <DefaultNavbar :action="{
+    route: 'javascript:;',
+    label: 'Buy Now',
+    color: 'btn-white',
+  }" transparent />
   <header class="bg-gradient-dark">
-    <div
-      class="page-header min-vh-50"
-      :style="{ backgroundImage: `url(${bg})` }"
-    >
+    <div class="page-header min-vh-50" :style="{ backgroundImage: `url(${bg})` }">
       <span class="mask bg-gradient-dark opacity-6"></span>
       <div class="container">
         <div class="row justify-content-center">
@@ -76,60 +70,42 @@ onUnmounted(() => {
 
   <div class="common-layout card card-body shadow-xl mx-5 mx-md-4 mt-n6 ">
     <el-container>
-      <el-aside width="300px" >
+      <el-aside width="300px">
         <div class="accordion ms-4" id="accordionExample">
           <!-- 手風琴 -->
           <div class="accordion-item">
             <router-link to="/pages/Yelu-pages/camp/North">
               <h2 class="accordion-header" id="headingOne">
-                <button
-                  class="accordion-button"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseOne"
-                  aria-expanded="true"
-                  aria-controls="collapseOne"
-                >
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                  aria-expanded="true" aria-controls="collapseOne">
                   <i class="material-icons ">home</i>北部營區 #NORTH CAMP
                 </button>
-              </h2></router-link>
-            <div
-              id="collapseOne"
-              class="accordion-collapse collapse show"
-              aria-labelledby="headingOne"
-              data-bs-parent="#accordionExample"
-            >
+              </h2>
+            </router-link>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample">
               <div class="word-body">
                 <div>
+                  <router-link></router-link>
                   <h6>新北 New Taipei</h6>
                   <h6>桃園 Taoyuan</h6>
                   <h6>新竹 Hsinchu</h6>
                 </div>
-              
+
               </div>
             </div>
           </div>
           <div class="accordion-item">
             <router-link to="/pages/Yelu-pages/camp/West">
               <h2 class="accordion-header" id="headingTwo">
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseTwo"
-                  aria-expanded="false"
-                  aria-controls="collapseTwo"
-                >
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                   <i class="material-icons">home</i>中部營區 #WEST CAMP
                 </button>
-              </h2></router-link
-            >
-            <div
-              id="collapseTwo"
-              class="accordion-collapse collapse"
-              aria-labelledby="headingTwo"
-              data-bs-parent="#accordionExample"
-            >
+              </h2>
+            </router-link>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+              data-bs-parent="#accordionExample">
               <div class="word-body">
                 <div>
                   <h6>苗栗 Miaoli</h6>
@@ -142,23 +118,14 @@ onUnmounted(() => {
           <div class="accordion-item">
             <router-link to="/pages/Yelu-pages/camp/South">
               <h2 class="accordion-header" id="headingThree">
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseThree"
-                  aria-expanded="false"
-                  aria-controls="collapseThree"
-                >
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                   <i class="material-icons">home</i>南部營區 #SOUTH CAMP
                 </button>
-              </h2></router-link>
-            <div
-              id="collapseThree"
-              class="accordion-collapse collapse"
-              aria-labelledby="headingThree"
-              data-bs-parent="#accordionExample"
-            >
+              </h2>
+            </router-link>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+              data-bs-parent="#accordionExample">
               <div class="word-body">
                 <div>
                   <h6>嘉義 Chiayi</h6>
@@ -171,24 +138,14 @@ onUnmounted(() => {
           <div class="accordion-item">
             <router-link to="/pages/Yelu-pages/camp/East">
               <h2 class="accordion-header" id="headingFour">
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseFour"
-                  aria-expanded="false"
-                  aria-controls="collapseFour"
-                >
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
                   <i class="material-icons">home</i>東部營區#EAST CAMP
                 </button>
-              </h2></router-link
-            >
-            <div
-              id="collapseFour"
-              class="accordion-collapse collapse"
-              aria-labelledby="headingFour"
-              data-bs-parent="#accordionExample"
-            >
+              </h2>
+            </router-link>
+            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour"
+              data-bs-parent="#accordionExample">
               <div class="word-body">
                 <div>
                   <h6>宜蘭 Yilan</h6>
@@ -202,8 +159,11 @@ onUnmounted(() => {
 
         <!-- 地圖 -->
         <div class="mt-5">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3634.7429887997278!2d120.77220251537011!3d24.355443871308932!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3469057690ed35a9%3A0xd5d012046ae76b77!2z5pa35qmL6YOo6JC977yO6LyV5aWi6Zyy54ef!5e0!3m2!1szh-TW!2stw!4v1679535747344!5m2!1szh-TW!2stw" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-          </div>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3634.7429887997278!2d120.77220251537011!3d24.355443871308932!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3469057690ed35a9%3A0xd5d012046ae76b77!2z5pa35qmL6YOo6JC977yO6LyV5aWi6Zyy54ef!5e0!3m2!1szh-TW!2stw!4v1679535747344!5m2!1szh-TW!2stw"
+            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
 
 
         <!-- 手風琴end -->
@@ -218,11 +178,10 @@ onUnmounted(() => {
 </template>
 
 <style>
-
-.word-body{
-display: flex;
-/* justify-content: center;
+.word-body {
+  display: flex;
+  /* justify-content: center;
 align-content: center; */
-margin-left: 30px;
+  margin-left: 30px;
 }
 </style>
